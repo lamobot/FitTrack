7942c0de4b16f36ec776f692cdc031a441465e45
Valerii Toropov <lamobot@Valeriis-MacBook-Pro.local>
2026-01-16 20:34:36 +0100

    Initial Commit

---
+++ FitTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitTrack
+//
+//  Created by Valerii Toropov on 16. 1. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitTrack/FitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackApp.swift
+//  FitTrack
+//
+//  Created by Valerii Toropov on 16. 1. 2026..
+//
+
+import SwiftUI
+
+@main
+struct FitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
