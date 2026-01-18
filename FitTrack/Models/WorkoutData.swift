5613a561be15bcef6c633b76cd9f6d1bb6ba1db8
Valerii Toropov <lamobot@Valeriis-MacBook-Pro.local>
2026-01-18 09:38:41 +0100

    edit exersizes

--- FitTrack/Models/WorkoutData.swift
+++ FitTrack/Models/WorkoutData.swift
@@ -22,12 +22,10 @@ struct WorkoutData {
             Exercise(name: "Жим в тренажёре", sets: 4, reps: 12, defaultWeight: 10)
         ]),
         ExerciseCategory(name: "Плечи", exercises: [
-            Exercise(name: "Жим сидя в тренажёре", sets: 3, reps: 12, defaultWeight: 20),
-            Exercise(name: "Разведения гантелей в стороны", sets: 3, reps: 15, defaultWeight: nil)
+            Exercise(name: "Жим сидя в тренажёре", sets: 3, reps: 12, defaultWeight: 20)
         ]),
         ExerciseCategory(name: "Трицепс", exercises: [
-            Exercise(name: "Разгибания на блоке (канат)", sets: 3, reps: 15, defaultWeight: 30),
-            Exercise(name: "Отжимания на брусьях в гравитроне", sets: 3, reps: 10, defaultWeight: nil)
+            Exercise(name: "Разгибания на блоке (канат)", sets: 3, reps: 15, defaultWeight: 30)
         ]),
         ExerciseCategory(name: "Кардио", exercises: [
             Exercise(name: "Ходьба", sets: 1, reps: 20, defaultWeight: nil)
@@ -44,16 +42,15 @@ struct WorkoutData {
             Exercise(name: "Боковая планка", sets: 3, reps: 20, defaultWeight: nil)
         ]),
         ExerciseCategory(name: "Спина", exercises: [
-            Exercise(name: "Вертикальная тяга широким хватом", sets: 4, reps: 12, defaultWeight: nil),
-            Exercise(name: "Горизонтальная тяга (блок к поясу)", sets: 4, reps: 12, defaultWeight: nil),
-            Exercise(name: "Рычажная тяга (хаммер)", sets: 3, reps: 12, defaultWeight: nil)
+            Exercise(name: "Вертикальная тяга широким хватом", sets: 4, reps: 12, defaultWeight: 30),
+            Exercise(name: "Горизонтальная тяга (блок к поясу)", sets: 4, reps: 12, defaultWeight: 30)
         ]),
         ExerciseCategory(name: "Бицепс", exercises: [
-            Exercise(name: "Сгибания на бицепс", sets: 3, reps: 12, defaultWeight: nil),
-            Exercise(name: "Сгибания «молот»", sets: 3, reps: 12, defaultWeight: nil)
+            Exercise(name: "Сгибания на бицепс", sets: 3, reps: 12, defaultWeight: 10),
+            Exercise(name: "Сгибания «молот»", sets: 3, reps: 12, defaultWeight: 10)
         ]),
         ExerciseCategory(name: "Задняя дельта", exercises: [
-            Exercise(name: "Разведения в тренажёре (обратная бабочка)", sets: 3, reps: 15, defaultWeight: nil)
+            Exercise(name: "Разведения в тренажёре (обратная бабочка)", sets: 3, reps: 15, defaultWeight: 20)
         ]),
         ExerciseCategory(name: "Кардио", exercises: [
             Exercise(name: "Ходьба", sets: 1, reps: 20, defaultWeight: nil)
@@ -72,7 +69,6 @@ struct WorkoutData {
         ExerciseCategory(name: "Ноги", exercises: [
             Exercise(name: "Жим ногами (платформа)", sets: 4, reps: 12, defaultWeight: 20),
             Exercise(name: "Разгибания ног", sets: 3, reps: 15, defaultWeight: 25),
-            Exercise(name: "Сгибания ног лёжа", sets: 3, reps: 15, defaultWeight: nil),
             Exercise(name: "Сведение ног", sets: 3, reps: 15, defaultWeight: 30),
             Exercise(name: "Разведение ног", sets: 3, reps: 15, defaultWeight: 35),
             Exercise(name: "Подъём на носки", sets: 3, reps: 20, defaultWeight: 10)
